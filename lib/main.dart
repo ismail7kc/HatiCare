5fc00474f745f1a607c0a4e69a7d2831ea2f0d29
usamaali-lab <usama.ali@7kctech.com>
2025-11-19 19:04:48 +0500

    Initial commit 19/11/2025

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         navigatorObservers: [ChuckerFlutter.navigatorObserver],
-        home: const SplashScreen(),
+        home: const PharmacyHomeScreen(),
       ),
     );
   }
