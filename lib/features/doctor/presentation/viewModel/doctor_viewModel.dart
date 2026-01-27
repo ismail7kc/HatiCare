51233fa5c4abaa3cb2b936e94a92db1450c25da2
dev7kingscode-hub <dev.7kingscode@gmail.com>
2026-01-27 19:25:42 +0500

    Working on Patient socket in Case of added or expire Patient.

--- lib/features/doctor/presentation/viewModel/doctor_viewModel.dart
+++ lib/features/doctor/presentation/viewModel/doctor_viewModel.dart
@@ -133,6 +133,9 @@ class DoctorViewModel extends ChangeNotifier {
                 _appointments.insert(0, AppointmentModel.fromJson(item));
                 _startQueueTimer();
                 shouldNotify = true;
+              } else if (decoded['type'] == 'patient_expiry') {
+                _handleExpiredPatient(decoded['patient']);
+                shouldNotify = true;
               }
 
               if (status == 'assigned') {
@@ -157,6 +160,16 @@ class DoctorViewModel extends ChangeNotifier {
       debugPrint("WS connect error: $e");
       _reconnect();
     }
+  }
+
+  void _handleExpiredPatient(Map<String, dynamic> patientJson) {
+    final expiredPatient = AppointmentModel.fromJson(patientJson);
+    _appointments.removeWhere((e) => e.id == expiredPatient.id);
+
+    Future.delayed(const Duration(seconds: 5), () {
+      _appointments.add(expiredPatient);
+      notifyListeners();
+    });
   }
 
   void _startQueueTimer() {
